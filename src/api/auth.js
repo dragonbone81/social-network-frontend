67e4ed36ecf387277b05e9427544b8af641044cc
Christian <cvernikoff@gmail.com>
2018-10-18 08:34:06 -0700

    netlify env var

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.BASE_URL ? process.env.BASE_URL : 'http://localhost:3001/';
+const BASE_URL = 'BASE_URL' in process.env ? process.env.BASE_URL : 'http://localhost:3001/';
 
 const register = async (user) => {
     const registerURL = BASE_URL + 'auth/register';
