a4b6de1ef5d7176ef37891124a308b9ce46fb919
Christian <cvernikoff@gmail.com>
2018-11-15 21:13:31 -0800

    gif stuff completed

--- src/Components/Chat/MessageList.js
+++ src/Components/Chat/MessageList.js
@@ -3,10 +3,18 @@ import {inject, observer} from "mobx-react";
 import Message from './Message';
 
 class MessageList extends Component {
+    state = {
+        slice: 20,
+    };
+
     render() {
         return (
             <>
-                {this.props.messages.map((message, index, arr) => {
+                {/*<div onClick={() => {*/}
+                    {/*this.setState({slice: this.state.slice + 20})*/}
+                {/*}}>Load More...*/}
+                {/*</div>*/}
+                {this.props.messages.slice().map((message, index, arr) => {
                     if (message.username === this.props.mainStore.user.username)
                         message.position = 'right';
                     else
